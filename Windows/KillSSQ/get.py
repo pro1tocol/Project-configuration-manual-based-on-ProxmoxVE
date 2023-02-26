21e5b3271e1a6c581ac4548beb947be5493a2507
dot1q <82779435+pro1tocol@users.noreply.github.com>
2023-02-26 13:39:19 +0800

    Update get.py

--- Windows/KillSSQ/get.py
+++ Windows/KillSSQ/get.py
@@ -11,14 +11,13 @@ def get_page_data(page):
 with open(r'./killssq.csv', 'w', encoding='utf-8-sig', newline='') as f:
     csv.writer(f).writerow(['中奖号码'])
 
-for i in range(0,150):  # 目前149页数据第
+for i in range(0,150):
     page = i
     df = get_page_data(page)
     columns_to_drop = [0, 1, 3, 4, 5]
     df.drop(df.columns[columns_to_drop], axis=1, inplace=True)
     print(f'正在抓取第{page * len(df)}条记录')
     
-    # 针对一列中每个元素处理: 去掉首部的 0，并在每个整数之后加上','
     for index, row in df.iterrows():
         lottery_data = [i for i in re.split(' +', row['中奖号码'].replace([' ' '-'], '').values[0])]
         lottery_data = [i.lstrip("0") + ',' for i in lottery_data]
