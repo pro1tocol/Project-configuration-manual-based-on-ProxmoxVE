f1db558cae5f339f462bead9954cea99e45f6728
dot1q <82779435+pro1tocol@users.noreply.github.com>
2023-03-30 18:22:47 +0800

    Create config.inc.php

---
+++ Docker/doker_web/typecho/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+// Debug Test
+define('__TYPECHO_DEBUG__', true);
+
+// HTTPS path
+define('__TYPECHO_SECURE__',true);
+
+// CDN path
+define('__TYPECHO_CDN_DIR__', 'https://cyber-mobile.net');
+
